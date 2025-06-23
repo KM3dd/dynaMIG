591ab41d8dad1d9ecec904d8342fde123ea632e4
KM3dd <jk_meddah@esi.dz>
2025-06-23 17:41:38 +0200

    planning

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,7 +5,12 @@ import (
 )
 
 func main() {
+	// usage :
+	// 1. Show status : GPUs lists with slices and indexes
+	// 2. list usable slices : - GPU i : indexes
+	// 3. create -g [GPU_ID] -p [profile-name] -s [start index]
+	// 4. delete -g [GPU_ID] -p [profile-name] -s [start index]
 
-	fmt.Println("This is dynamic mig manager")
+	fmt.Println("This is dynamic mig manager !")
 
 }
